fa2f53dc25c18b5cd1eb807c3798d569bcbd3e01
jararugu <113119300+jararugu@users.noreply.github.com>
2023-03-28 11:57:33 +0530

    Add files via upload

---
+++ Pattern8.java
@@ -0,0 +1,17 @@
+class Pattern8{
+    public static void main(String[] args) {
+      int  k=2;
+      for(int i=1;i<=5;i++)
+      {
+          for(int j=1;j<=5;j++)
+          {
+              System.out.printf("%3d",k);
+                k += 2;
+  
+          }
+          System.out.println("");
+      }
+    }
+  
+  
+  }
